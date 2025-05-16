ab5f4ed00289d81a6731053dc16d11ae709963f4
unknown <yashic089@gmail.com>
2025-05-16 14:30:50 +0530

    print(assignments)

---
+++ assignment.py
@@ -0,0 +1,166 @@
+#FIRST ASSIGNMENT.
+
+#QUESTION-1
+# print("HELLO WORLD!!")
+
+#QUS-2
+# name=input("ENTER YOUR NAME:")
+# print("HELLO!",name)
+
+#Q-3
+# numb_1= float(input("ENTER NUMBER :"))
+# if numb_1 > 0:
+#     print("NUMBER IS POSITIVE.")
+# elif numb_1 < 0:
+#     print("NUMBER IS NEGATIVE.")
+# else:
+#  print("NUMBER IN ZERO.")
+
+#Q-4
+# 
+# a=float(input("ENTER A =")) 
+# b=float(input("ENTER B =")) 
+# c=float(input("ENTER C =")) 
+# if a>=b and a>=c:
+#     print("A IS LARGEST.")
+# elif b>=a and b>=c:
+#     print("B IS LARGEST.")
+# else:
+#     print("C IS LARGEST.")
+
+#Q-5
+# num = int(input("Enter a positive number: "))
+
+# if num < 0:
+#     print("ENTERED A NEGATIVE NUMBER!!.")
+# else:
+#     factorial = 1
+
+#     for i in range(1, num + 1):
+#         factorial *= i
+#     print(f"The factorial of {num} is {factorial}")
+
+
+#Q-6
+# a=("MY NAME IS YASHI.")
+# A=245
+# b=245.667
+# B=True
+# c=False
+# print(a)
+# print(A)
+# print(b)
+# print(B)
+# print(c)
+# print(type(a))
+# print(type(A))
+# print(type(b))
+# print(type(B))
+# print(type(c))
+
+#Q-7
+# first=input('ENTER FIRST :')
+# second=input("ENTER SECOND :")
+# first,second= second,first
+# print("After Change First =",first)
+# print("After Change Second =",second)
+
+#Q-9
+# string1= input("ENTER FIRST:")
+# string2= input("ENTER SECOND:")
+# final_s= string1 + string2
+# print("YOUR STRING :",final_s)
+
+#Q-10
+# a =input("ENTER THE VALUE=")
+# if   str(a) 
+
+#Q-11
+# add= 10+40
+# subt= 40-10
+# multiply= 10*40
+# division= 40/10
+# print(add)
+# print(subt)
+# print(multiply)
+# print(division)
+
+#Q-12
+# a= 100
+# b= 500
+# equal= (a==b)
+# print(equal)
+# not_1 = (a!=b)
+# print(not_1)
+# greater = (a>b)
+# print(greater)
+# less= (a<b)
+# print(less)
+# g_2= (a>=b)
+# print(g_2)
+# l_2= (a<=b)
+# print(l_2)
+
+#Q-13
+# a= float(input("ENTER A:"))
+# b= float(input("ENTER B:"))
+# opt_1= (a>b and a<b)
+# opt_2= (a>b or a<b)
+# opt_3= not(a>b ,  a<b)
+# print(opt_1)
+# print(opt_2)
+# print(opt_3)
+
+#Q-14
+# numb=float(input("ENTER NUMBER :"))
+# sq= (numb**2)
+# print("SQUARE OF YOUR NUMBER:", sq)
+
+#q-15
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print(f"Number is even {num}.")
+# else:
+#     print(f"Number is odd {num}.")
+
+#Q-16
+# y = int(input("Enter a positive number: "))
+# if y <= 0:
+#     print("ENTER POSITIVE.")
+# else:
+#     total = y * (y + 1) // 2  
+#     print("The sum of the first natural numbers is:", total)
+
+#Q-17
+# year = int(input("ENTER A YEAR:"))
+# if year/4 ==0:
+#  print(f"{year} IS A LEAP YEAR.")
+
+# else:
+#     print(f"{year} IS NOT A LEAP YEAR.")
+
+
+
+#Q-18
+# string1= input("ENTER STRING:")
+# reversed=string1[::-1]
+# print("REVERSE STRING:",reversed)
+# print(f"THE STRING IS:{reversed}")
+
+#Q-19
+# string=input("ENTER A STRING=")
+# string_111= string.replace(" ","").lower()
+# if string_111==string:
+#     print("THIS IS A PALINDROME.")
+# else:
+#     print("THIS IS NOT A PALINDROME.")
+
+
+#Q-20
+# list1= ['YASHI','Hello',"True", 'False', "Hiee"]
+# list2= [1,8,9,239178,36892,278,4619,7,8,5467,10]
+# list1.sort()
+# list2.sort()
+# print(list1)
+# print(list2)
+
